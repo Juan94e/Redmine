cceca92d488f3cb1f07f5296ae192daf67d7120b
Juan Espinosa <juan94e@gmail.com>
2025-03-13 22:46:26 -0500

    cambio en App.jsx

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -12,6 +12,9 @@ const App = () => {
     return (
         <Router>
             <Routes>
+
+                <Route path="/" element={<Navigate to="/login" replace />} />
+
                 {/* Ruta pública */}
                 <Route path="/login" element={<Login />} />
 
@@ -35,7 +38,7 @@ const App = () => {
                 </Route> */}
 
                 {/* Redirección para rutas no definidas */}
-                <Route path="*" element={<Navigate to="/" replace />} />
+                {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
             </Routes>
         </Router>
     );
